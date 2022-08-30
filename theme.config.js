3555b047e522cf655e8342aad6273f26f6f0cf1a
Max Mohammadi <maxmohammadi@gmail.com>
2022-08-29 23:46:39 -0700

    changed links/descriptions

--- theme.config.js
+++ theme.config.js
@@ -22,14 +22,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Seam Money: Structured Yield Aggregator on Aptos" />
+      <meta name="og:description" content="Seam Money: Structured Yield Aggregator on Aptos" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:image" content="./public/seam-thumbnail.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="og:title" content="Seam Money: Structured Yield Aggregator on Aptos" />
+      <meta name="og:image" content="./public/seam-thumbnail.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
@@ -68,6 +68,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Seam Money.</>,
-  unstable_faviconGlyph: '👋',
+  footerText: <>MIT {new Date().getFullYear()} © Seam Labs, LLC.</>,
+  unstable_faviconGlyph: '📄',
 }
